f4e4f96312f1befa0dc956acf69cadd03ac2ef6d
Melissa Rose <123melissa321@gmail.com>
2022-07-13 13:04:30 +0100

    Fixed formatting issues

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -4,7 +4,8 @@ function PageHeader(): JSX.Element {
       <h1>Stan BTS</h1>
       <p>Welcome to Melissa's Web Page!</p>
       <p>
-        This shows a random scatter of my thoughts, last updated on July 13th, 2022.
+        This shows a random scatter of my thoughts, last updated on July 13th,
+        2022.
       </p>
     </header>
   );
